a2fc592b0adf0ceb6134cb9014aa2b3c0d75c07b
tatitati <francis.jaa@protonmail.com>
2024-03-16 12:12:32 +0100

    cleaning

--- timestamp_to_datetime.js
+++ timestamp_to_datetime.js
@@ -16,17 +16,10 @@ var date = new Date(Number(unix_timestamp) * 1000);
 var year = date.getFullYear();
 var month = date.getMonth();
 var day = date.getDay();
-
-// Hours part from the timestamp
 var hours = date.getHours();
-
-// Minutes part from the timestamp
 var minutes = "0" + date.getMinutes();
-
-// Seconds part from the timestamp
 var seconds = "0" + date.getSeconds();
 
-// Will display time in 10:30:23 format
 var result = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
 
